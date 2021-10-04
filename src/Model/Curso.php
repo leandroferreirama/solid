4f2245b84e03721e938388366ceffd60d77a08ce
Leandro Ferreira Marcelli <suporte@integracaosistema.com.br>
2021-10-04 19:21:16 -0300

    Implementado uma interface para eliminar os ifs

--- src/Model/Curso.php
+++ src/Model/Curso.php
@@ -2,7 +2,7 @@
 
 namespace Alura\Solid\Model;
 
-class Curso
+class Curso implements Pontuavel
 {
     private $nome;
     private $videos;
@@ -34,5 +34,10 @@ class Curso
     public function recuperarVideos(): array
     {
         return $this->videos;
+    }
+
+    public function recuperaPontuacao(): int
+    {
+        return 100;
     }
 }
